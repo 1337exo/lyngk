c1a58e8e51efc355cfb7ebb5df271fe22656e7c3
exoleet <exoleet@exoleet.home>
2017-10-18 15:29:13 +0200

    Test first story

--- test/test.js
+++ test/test.js
@@ -1,3 +1,14 @@
+/*** MENARD LUDOVIC ***/
+/***** 18/10/2017 *****/
+
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+
+/*** STORY 1 ***/
+
+LyngkTestCase.prototype.testStory1 = function () {
+  var coordinates = new Lyngk.coordinates('A', 1);
+  asserFalse(coordinates.is_valide());
+};
